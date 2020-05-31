6088482b48a88e51f2ccf27d4bf2154429e9f6e3
Mona Lisa <david.t.bui83@gmail.com>
2020-05-31 14:25:25 -0700

    make it better

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({extended: false}));
 
-const PORT = process.env.PORT || 5000;
-
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
@@ -23,5 +21,7 @@ if(process.env_NODE_ENV === 'production'){
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {console.log(`listening on port ${PORT}`)});
